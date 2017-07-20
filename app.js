a2ddfe60ff368673574bc29280cfddc185b5259d
burningserenity <burningserenity@bitmai.la>
2017-07-20 19:25:10 -0400

    push code to Firebase

---
+++ app.js
@@ -0,0 +1,36 @@
+var config = {
+    apiKey: "AIzaSyC-YLamtqLZoJCMHOIbbut85kjQ1weLcUs",
+    authDomain: "billiableproject.firebaseapp.com",
+    databaseURL: "https://billiableproject.firebaseio.com",
+    projectId: "billiableproject",
+    storageBucket: "",
+    messagingSenderId: "1054135583104"
+  };
+
+   // Create a variable to reference the database
+
+   firebase.initializeApp(config);
+
+   var database = firebase.database();
+
+$(".submit").on("click", function (event) {
+
+        // Don't refresh the page!
+        event.preventDefault();
+
+        var employeeName= $("#employeeName").val().trim();
+        var role = $("#role").val().trim();
+        //takes the input val from #firstTrainTime parses it out as HH:mm and then formats it as a unix time stamp
+        var startDate = $("#startDate").val().trim();
+        var monthlyRate = $("#monthlyRate").val().trim();
+
+
+
+        database.ref().push({
+            employeeName: employeeName,
+            role: role,
+            startDate: startDate,
+            monthlyRate: monthlyRate
+        });
+
+    })
