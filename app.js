f45e6ece6ff4be9aee6dd82dd4263ac320866d64
burningserenity <burningserenity@bitmai.la>
2017-07-20 20:32:44 -0400

    error checking in app.js

--- app.js
+++ app.js
@@ -5,25 +5,27 @@ var config = {
     projectId: "billiableproject",
     storageBucket: "",
     messagingSenderId: "1054135583104"
-  };
+};
 
-   // Create a variable to reference the database
+// Create a variable to reference the database
 
-   firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-   var database = firebase.database();
+var database = firebase.database();
 
 $(".submit").on("click", function (event) {
 
-        // Don't refresh the page!
-        event.preventDefault();
+    // Don't refresh the page!
+    event.preventDefault();
 
-        var employeeName= $("#employeeName").val().trim();
-        var role = $("#role").val().trim();
-        var startDate = $("#startDate").val().trim();
-        var monthlyRate = $("#monthlyRate").val().trim();
+    var employeeName = $("#employeeName").val().trim();
+    var role = $("#role").val().trim();
+    var startDate = $("#startDate").val().trim();
+    var monthlyRate = $("#monthlyRate").val().trim();
 
 
+
+    if (employeeName != "" && role != "" && startDate != "" && monthlyRate != "") {
 
         database.ref().push({
             employeeName: employeeName,
@@ -31,25 +33,27 @@ $(".submit").on("click", function (event) {
             startDate: startDate,
             monthlyRate: monthlyRate
         });
-    
-    if (employeeName != "" && role != "" && startDate != "" && monthlyRate != "") {
+
+
         $("#employeeName").val("");
         $("#role").val("");
         $("#startDate").val("");
         $("#monthlyRate").val("");
+
+
+    } else {
+        alert("You are missing data!");
     }
 
-    })
+})
 
-    database.ref().on('child_added', function(childSnapshot){
+database.ref().on('child_added', function (childSnapshot) {
 
 
-      $("tbody").append("<tr><td id='employee'> " + childSnapshot.val().employeeName +
+    $("tbody").append("<tr><td id='employee'> " + childSnapshot.val().employeeName +
         " </td><td id='roleVal'> " + childSnapshot.val().role +
         " </td><td id='beginDate'> " + childSnapshot.val().startDate +
-        " </td><td id='rateMonthly'> " + childSnapshot.val().monthlyRate + "</td></tr>")
-    })
-
-
-
-
+        " </td><td id='monthWorked'> " + childSnapshot.val().startDate +
+        " </td><td id='rateMonthly'> " + childSnapshot.val().monthlyRate +
+        " </td><td id='totalBill'> " + childSnapshot.val().startDate + "</td></tr>")
+})
